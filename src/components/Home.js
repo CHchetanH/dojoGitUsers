0aec838c0c5d4984340578ea66ca72eb24983b00
Chetan Hatnapure <chchetanrules7@gmail.com>
2021-12-18 12:15:16 +0530

    8.useState

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,22 @@
+import { useState } from "react";
 const Home = () => {
+	const [name, setName] = useState("Chetan");
 	const handleClick = () => {
-		console.log("Clicked");
-	};
-	const handleClickAgain = (e, name) => {
-		console.log(e.target);
-		console.log(name);
+		// setName("Minal");
+		setName(() => {
+			if (name === "Chetan") {
+				return "Minal";
+			} else if (name === "Minal") {
+				return "Chetan";
+			}
+		});
 	};
 
 	return (
 		<div className="home">
 			<h1>Home</h1>
+			<h3>{name}</h3>
 			<button onClick={handleClick}>Click Me </button>
-			<button
-				onClick={(e) => {
-					handleClickAgain(e, "chetan");
-				}}
-			>
-				Click Me{" "}
-			</button>
 		</div>
 	);
 };
