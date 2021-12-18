ea47fd6cb2a3d67f3cac3d3da499943defb32abd
Chetan Hatnapure <chchetanrules7@gmail.com>
2021-12-18 12:45:36 +0530

    getting list and mapping all users on homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,20 @@
 import { useState } from "react";
 const Home = () => {
-	const [name, setName] = useState("Chetan");
-	const handleClick = () => {
-		// setName("Minal");
-		setName(() => {
-			if (name === "Chetan") {
-				return "Minal";
-			} else if (name === "Minal") {
-				return "Chetan";
-			}
-		});
-	};
+	const [name, setName] = useState([
+		{ id: 1, name: "chetan", city: "chandrapur", age: 27 },
+		{ id: 2, name: "minal", city: "pune", age: 31 },
+		{ id: 3, name: "snehal", city: "pune", age: 33 },
+	]);
 
 	return (
 		<div className="home">
-			<h1>Home</h1>
-			<h3>{name}</h3>
-			<button onClick={handleClick}>Click Me </button>
+			{name.map((e) => (
+				<div className="alluser" key={e.id}>
+					<h2>{e.name}</h2>
+					<h3>Lives in: {e.city}</h3>
+					<h4>{e.age} year Old</h4>
+				</div>
+			))}
 		</div>
 	);
 };
