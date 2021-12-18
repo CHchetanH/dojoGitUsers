783b5ae440704b970e1cf6b8c9ab54fd539121f2
Chetan Hatnapure <chchetanrules7@gmail.com>
2021-12-18 12:04:08 +0530

    click event and function as dynamic values in template

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,25 @@
 const Home = () => {
-	return <div className="home">Home</div>;
+	const handleClick = () => {
+		console.log("Clicked");
+	};
+	const handleClickAgain = (e, name) => {
+		console.log(e.target);
+		console.log(name);
+	};
+
+	return (
+		<div className="home">
+			<h1>Home</h1>
+			<button onClick={handleClick}>Click Me </button>
+			<button
+				onClick={(e) => {
+					handleClickAgain(e, "chetan");
+				}}
+			>
+				Click Me{" "}
+			</button>
+		</div>
+	);
 };
 
 export default Home;
